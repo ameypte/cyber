d4eba202eced308dfc63f18bcb1f1a56321afa4b
ameypte <ameypte@gmail.com>
2024-03-03 01:51:36 +0530

    Fix DB_PASSWORD typo and add user authentication to NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,20 @@
-import React from 'react'
+"use client";
+import { React, useEffect, useState } from 'react'
 import Link from 'next/link';
 
 export default function NavBar() {
+
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const user = localStorage.getItem('user');
+        if (user) {
+            setUser(JSON.parse(user));
+        }
+    }, []);
+
+
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -10,7 +23,19 @@ export default function NavBar() {
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SecureEdge</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <Link href="/auth/login" type="button" className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Get started</Link>
+                    {
+                        user ?
+                                <div className="flex items-center space-x-3 rtl:space-x-reverse">
+                                    <span className="text-gray-900 dark:text-white">Welcome, {user.name}</span>
+                                    <button type="button" className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800" onClick={() => {
+                                        localStorage.removeItem("user");
+                                        setUser(null);
+                                    }
+                                    }>Logout</button>
+                                </div>
+                            :
+                            <Link href="/auth/login" type="button" className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Get started</Link>
+                    }
                     <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">

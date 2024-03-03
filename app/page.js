0bf6c7417e14319abac904d3ac7156f714e008b8
ameypte <ameypte@gmail.com>
2024-03-03 13:00:10 +0530

    Add Card component and update ProgressBar component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Card from "@/components/Card";
 import IntoBanner from "@/components/IntroBanner";
 import NavBar from "@/components/NavBar";
 import ProgressBar from "@/components/ProgressBar";
@@ -6,7 +7,16 @@ export default function Home() {
     <div className="bg-gray-50 dark:bg-gray-900">
       <NavBar />
       <IntoBanner />
-      <ProgressBar />
+      <div className="max-w-screen-xl mx-auto">
+        <ProgressBar progress={40} />
+        <div className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 lg:grid-cols-3">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
     </div>
   );
 }

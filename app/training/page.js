8c5d12e0830b76f7c995bdb51b9dcbaafcca881d
ameypte <ameypte@gmail.com>
2024-03-03 16:46:58 +0530

    Update function to handle edge cases

---
+++ app/training/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className=" bg-white rounded-lg dark:bg-gray-800 p-4 shadow-lg">
+        dskhjdfh
+    </div>
+  )
+}

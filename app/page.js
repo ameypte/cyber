64ae41c3c81f01fd31343decfe468fee59d1fcda
heramb_45 <heramb.bhoodhar@gmail.com>
2024-03-03 16:44:33 +0530

    Title and Card Added

--- app/page.js
+++ app/page.js
@@ -7,14 +7,23 @@ export default function Home() {
     <div className="bg-gray-50 dark:bg-gray-900">
       <NavBar />
       <IntoBanner />
+      {/* <hr /> */}
       <div className="max-w-screen-xl mx-auto">
-        <ProgressBar progress={40} />
+        <ProgressBar progress={75} />
+        <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+          <h2 class="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white">
+            Empower Your Cybersecurity Knowledge
+          </h2>
+          <p class=" text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+            Track Your Cybersecurity Journey: Monitor Progress and Earn Your
+            Certificate
+          </p>
+        </div>
         <div className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 lg:grid-cols-3">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Card img={"/img2.jpg"} />
+          <Card img={"/img2.jpg"} />
+          <Card img={"/img2.jpg"} />
+          <Card img={"/img2.jpg"} />
         </div>
       </div>
     </div>

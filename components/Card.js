31d025680b070b04cf8a8534bcec7f8eaed213d3
ameypte <ameypte@gmail.com>
2024-03-04 10:01:12 +0530

    Update Card.js href link

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,7 @@ export default function Card({ img, title, description }) {
   return (
     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <a
-        href="#"
+        href="training/phishing"
         class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <img

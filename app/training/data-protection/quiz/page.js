d8c7c5794e04bee37128d4b5e4c8b374a5ba17db
heramb_45 <heramb.bhoodhar@gmail.com>
2024-03-26 22:00:48 +0530

    aas

--- app/training/data-protection/quiz/page.js
+++ app/training/data-protection/quiz/page.js
@@ -7,7 +7,7 @@ import Progress from "@/components/PhishingQuiz/Progress";
 
 import FinishedScreen from "@/components/PhishingQuiz/FinishedScreen";
 // const [loading, setLoading] = useState(false);
-const PhishingData = require("./phishing.json");
+const DataProtection = require("./dataprotection.json");
 
 export default function page() {
   const [index, setIndex] = useState(0);
@@ -16,7 +16,8 @@ export default function page() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showModal, setShowModal] = useState(false);
 
-  const length = PhishingData?.questions?.length || 0;
+  const length = DataProtection?.questions?.length || 0;
+
   const ResetQuiz = () => {
     setCurrentQuestion(() => 0);
     setAnswer(() => null);
@@ -24,7 +25,7 @@ export default function page() {
     setShowModal(() => false);
   };
   useEffect(() => {
-    if (!PhishingData || !PhishingData.questions) {
+    if (!DataProtection || !DataProtection.questions) {
       setError("Phishing data is missing or invalid");
     }
   }, []);
@@ -51,14 +52,14 @@ export default function page() {
 
           <div style={{ backdropFilter: "inherit" }}>
             <Question
-              question={PhishingData.questions[currentQuestion]}
+              question={DataProtection.questions[currentQuestion]}
               correctAnswer={
-                PhishingData.questions[currentQuestion].correctOption
+                DataProtection.questions[currentQuestion].correctOption
               }
               answer={answer}
               setAnswer={setAnswer}
               totalPoints={setPoints}
-              points={PhishingData.questions[currentQuestion].points}
+              points={DataProtection.questions[currentQuestion].points}
               setCurrentQuestion={setCurrentQuestion}
             />
             {answer !== null && (

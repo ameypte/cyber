7011bf325e8b7f95250cae318e5aae8e3dc29ba9
ameypte <ameypte@gmail.com>
2024-03-26 19:57:52 +0530

    password genetor page added

---
+++ app/training/pass-security/quiz/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    
+    <div class="pb-4 mb-8 border-b border-gray-200 dark:border-gray-800">
+        <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-100">
+            Quiz page
+        </h1>
+    </div>
+  )
+}
